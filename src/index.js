51f8c770098bec4a429840d369a99fd3802cf722
Timotej Avsec <avsectimotej1@gmail.com>
2020-06-10 23:04:37 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import VueTree from "./components/VueTree.vue";
+
+export default {
+    install(Vue, options) {
+        // Let's register our component globally
+        // https://vuejs.org/v2/guide/components-registration.html
+        Vue.component("vue-tree", VueTree);
+    }
+};
